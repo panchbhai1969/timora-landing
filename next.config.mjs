12b0cd00ef63501c0ca1c212c46dc7432fa9f152
Anand <panchbhai1969@gmai.com>
2025-02-21 00:55:47 +0530

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: "export",
+    distDir: "out", // Output folder
+    images: {
+      unoptimized: true, // Required for images to work on static hosting
+    },
+  };
 export default nextConfig;
